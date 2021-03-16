6c606a23c90cba9d68973356c592abebc63d4004
Francesco Arces <s256537@studenti.polito.it>
2021-03-16 17:44:57 +0100

    Soluzione punto 1

---
+++ src/main/java/it/polito/tdp/librettovoti/model/Voto.java
@@ -0,0 +1,57 @@
+package it.polito.tdp.librettovoti.model;
+
+import java.time.LocalDate;
+
+
+// POJO - Plain Old Java Object
+// Java Bean
+
+/**
+ * Memorizza il risultato di un esame singolo
+ * 
+ * @author User
+ *
+ */
+public class Voto {
+	private String nome;
+	private int voto; //30L ???
+	private LocalDate data; //data superamento esame
+	
+	public Voto(String nome, int voto, LocalDate data) {
+		super();
+		this.nome = nome;
+		this.voto = voto;
+		this.data = data;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getVoto() {
+		return voto;
+	}
+
+	public void setVoto(int voto) {
+		this.voto = voto;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	@Override
+	public String toString() {
+		return "Esame " + nome + " superato con " + voto + " il " + data;
+	}
+	
+
+}
