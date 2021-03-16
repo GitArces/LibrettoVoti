a403b8dbecbf4871cf94917cf646c3603d114618
Francesco Arces <s256537@studenti.polito.it>
2021-03-16 18:30:16 +0100

    Parziale punto 3

--- src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
@@ -49,6 +49,25 @@ public class Libretto {
 		return risultato;
 	}
 	
+	
+	/**
+	 * Ricerca un Voto del corso di cui è specificato il nome
+	 * Se non lo trova restituisce null.
+	 * @param nomeCorso
+	 * @return
+	 */
+	public Voto ricercaCorso (String nomeCorso) {
+		Voto risultato = null;
+		for (Voto v : this.voti) {
+			if (v.getNome().equals(nomeCorso)) {
+				risultato = v;
+				break;
+			}
+		}
+		return risultato;
+	}
+	
+	
 	public String toString() {
 		String s = "";
 		for (Voto v : this.voti) {
